3cb5667f3b4b579d0452c6128c2b91ce09e2eb28
Modar Nasser <modar1999@gmail.com>
2022-02-22 23:24:29 +0100

    make EntityDef inherit from TagsContainer make TagsContainer methods const

--- src/EntityDef.cpp
+++ src/EntityDef.cpp
@@ -6,12 +6,12 @@
 using namespace ldtk;
 
 EntityDef::EntityDef(const nlohmann::json& j, World* w) :
+TagsContainer(j["tags"]),
 name(j["identifier"].get<std::string>()),
 uid(j["uid"].get<int>()),
 size(j["width"].get<int>(), j["height"].get<int>()),
 color(j["color"].get<std::string>()),
 pivot(j["pivotX"].get<float>(), j["pivotY"].get<float>()),
 tileset(j["tileRect"].is_null() ? nullptr : &w->getTileset(j["tileRect"]["tilesetUid"].get<int>())),
-texture_rect(j["tileRect"].is_null() ? IntRect{} : IntRect{j["tileRect"]["x"], j["tileRect"]["y"], j["tileRect"]["w"], j["tileRect"]["h"]}),
-tags(j["tags"].get<std::vector<std::string>>())
+texture_rect(j["tileRect"].is_null() ? IntRect{} : IntRect{j["tileRect"]["x"], j["tileRect"]["y"], j["tileRect"]["w"], j["tileRect"]["h"]})
 {}
