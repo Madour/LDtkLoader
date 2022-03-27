92bf0900cb86732cb584e8b312df0d73849363e8
Modar Nasser <modar1999@gmail.com>
2022-03-27 13:59:12 +0200

    FieldsContainer: add getField<FieldType> to get field using enum value and name

---
+++ include/LDtkLoader/Field.hpp
@@ -0,0 +1,88 @@
+// Created by Modar Nasser on 23/03/2022.
+
+#pragma once
+
+#include <vector>
+#include <iostream>
+
+#include "LDtkLoader/thirdparty/optional.hpp"
+
+namespace ldtk {
+    template<typename T>
+    using optional = std::experimental::optional<T>;
+
+    constexpr auto null = std::experimental::nullopt;
+
+    struct IField {
+        virtual ~IField() = default;
+    };
+
+    template <typename T>
+    struct Field;
+
+    template <typename T>
+    struct ArrayField;
+
+    template <typename T>
+    struct Field : IField, private optional<T> {
+        using value_type = T;
+
+        using optional<T>::optional;
+        using optional<T>::value;
+        using optional<T>::value_or;
+        using optional<T>::operator->;
+
+        constexpr auto is_null() const -> bool {
+            return !optional<T>::has_value();
+        }
+
+        constexpr operator ArrayField<T>&() const {
+            throw std::runtime_error("Cannot convert ldtk::Field<T> to ldtk::ArrayField<T>");
+            //return ArrayField<T>(std::vector<Field<T>>(*this));
+        }
+
+    };
+
+    template <class T>
+    constexpr bool operator==(const Field<T>& lhs, const T& rhs) {
+        if (lhs.is_null()) return false;
+        return (lhs.value() == rhs);
+    }
+
+    template <class T>
+    constexpr bool operator==(const T& lhs, const Field<T>& rhs) {
+        if (rhs.is_null()) return false;
+        return (rhs.value() == lhs);
+    }
+
+    template <class T>
+    constexpr bool operator!=(const Field<T>& lhs, const T& rhs) {
+        return !(lhs == rhs);
+    }
+
+    template <class T>
+    constexpr bool operator!=(const T& lhs, const Field<T>& rhs) {
+        return !(lhs == rhs);
+    }
+
+    template <typename T>
+    struct ArrayField : IField, std::vector<Field<T>> {
+        using value_type = T;
+        ArrayField() = default;
+        explicit ArrayField(const std::vector<Field<T>>& vals) : std::vector<Field<T>>(vals) {}
+
+        constexpr operator Field<T>&() const {
+            throw std::runtime_error("Cannot convert ldtk::ArrayField<T> to ldtk::Field<T>");
+            // return this->at(0);
+        }
+    };
+}
+
+template <typename T>
+auto operator<<(std::ostream& os, ldtk::Field<T> field) -> std::ostream& {
+    if (field.is_null())
+        os << "null";
+    else
+        os << field.value();
+    return os;
+}
--- include/LDtkLoader/containers/FieldsContainer.hpp
+++ include/LDtkLoader/containers/FieldsContainer.hpp
@@ -9,65 +9,29 @@
 #include "LDtkLoader/thirdparty/json.hpp"
 #include "LDtkLoader/defs/FieldDef.hpp"
 #include "LDtkLoader/DataTypes.hpp"
+#include "LDtkLoader/Field.hpp"
 #include "LDtkLoader/Utils.hpp"
 
 namespace ldtk {
     class World;
 
-    template<typename T>
-    using optional = std::experimental::optional<T>;
-
-    constexpr auto null = std::experimental::nullopt;
-
-    struct IField {
-        virtual ~IField() = default;
-    };
-
-    template <typename T>
-    struct Field : IField, private optional<T> {
-
-        using optional<T>::optional;
-        using optional<T>::value;
-        using optional<T>::value_or;
-        using optional<T>::operator->;
-
-        constexpr auto is_null() const -> bool {
-            return !optional<T>::has_value();
-        }
-
-    };
-
-    template <class T>
-    constexpr bool operator==(const Field<T>& lhs, const T& rhs) {
-        if (lhs.is_null()) return false;
-        return (lhs.value() == rhs);
-    }
-
-    template <class T>
-    constexpr bool operator==(const T& lhs, const Field<T>& rhs) {
-        if (rhs.is_null()) return false;
-        return (rhs.value() == lhs);
-    }
-
-    template <class T>
-    constexpr bool operator!=(const Field<T>& lhs, const T& rhs) {
-        return !(lhs == rhs);
-    }
-
-    template <class T>
-    constexpr bool operator!=(const T& lhs, const Field<T>& rhs) {
-        return !(lhs == rhs);
-    }
-
-    template <typename T>
-    struct ArrayField : IField, std::vector<Field<T>> {
-        ArrayField() = default;
-        explicit ArrayField(const std::vector<Field<T>>& vals) : std::vector<Field<T>>(vals) {}
-    };
-
     class FieldsContainer {
     public:
         FieldsContainer() = default;
+
+        template <FieldType T>
+        auto getField(const std::string& name) const -> const getFieldType<T>&;
+
+        template <typename T>
+        auto getField(const std::string& name) const -> const Field<T>&;
+
+        template <typename T>
+        auto getArrayField(const std::string& name) const -> const ArrayField<T>&;
+
+    protected:
+        FieldsContainer(const nlohmann::json& j, const World* w);
+
+        void parseFields(const nlohmann::json& j, const World* w);
 
         template <typename T>
         void addField(const std::string& name, const T& field);
@@ -76,26 +40,10 @@ namespace ldtk {
         void addField(const std::string& name, const Field<T>& field);
 
         template <typename T>
-        auto getField(const std::string& name) const -> const Field<T>&;
-
-        template <FieldType T>
-        auto getField(const std::string& name) const -> const Field<getFieldType<T>>&;
-
-        template <typename T>
         void addArrayField(const std::string& name, const std::vector<Field<T>>& field);
 
         template <typename T>
         void addArrayField(const std::string& name, const ArrayField<T>& field);
-
-        template <typename T>
-        auto getArrayField(const std::string& name) const -> const ArrayField<T>&;
-
-        template <FieldType T>
-        auto getArrayField(const std::string& name) const -> const ArrayField<getFieldType<T>>&;
-
-    protected:
-        FieldsContainer(const nlohmann::json& j, const World* w);
-        void parseFields(const nlohmann::json& j, const World* w);
 
     private:
         std::vector<std::unique_ptr<IField>> m_gc;
@@ -103,6 +51,40 @@ namespace ldtk {
         std::unordered_map<std::string, IField*> m_array_fields;
     };
 
+    template <FieldType T>
+    auto FieldsContainer::getField(const std::string& name) const -> const getFieldType<T>& {
+        using FieldT = getFieldType<T>;
+        const auto is_array = std::is_base_of<FieldT, ArrayField<typename FieldT::value_type>>::value;
+        if (is_array) {
+            return getArrayField<typename FieldT::value_type>(name);
+        } else {
+            return getField<typename FieldT::value_type>(name);
+        }
+    }
+
+    template <typename T>
+    auto FieldsContainer::getField(const std::string& name) const -> const Field<T>& {
+        if (m_fields.count(name) > 0) {
+            const auto* field = m_fields.at(name);
+            const auto* ret = dynamic_cast<const Field<T>*>(field);
+            if (ret)
+                return *ret;
+            ldtk_error("Field \"" + name + "\" is not of type " + typeid(T).name() + ".");
+        }
+        ldtk_error("Field \"" + name + "\" does not exist.");
+    }
+
+    template <typename T>
+    auto FieldsContainer::getArrayField(const std::string& name) const -> const ArrayField<T>& {
+        if (m_array_fields.count(name) > 0) {
+            const auto* field = m_array_fields.at(name);
+            const auto* ret = dynamic_cast<const ArrayField<T>*>(field);
+            if (ret)
+                return *ret;
+            ldtk_error("ArrayField \"" + name + "\" is not of type " + typeid(T).name() + ".");
+        }
+        ldtk_error("ArrayField \"" + name + "\" does not exist.");
+    }
 
     template <typename T>
     void FieldsContainer::addField(const std::string& name, const T& field) {
@@ -119,23 +101,6 @@ namespace ldtk {
     }
 
     template <typename T>
-    auto FieldsContainer::getField(const std::string& name) const -> const Field<T>& {
-        if (m_fields.count(name) > 0) {
-            const auto* field = m_fields.at(name);
-            const auto* ret = dynamic_cast<const Field<T>*>(field);
-            if (ret)
-                return *ret;
-            ldtk_error("Field \"" + name + "\" is not of type " + typeid(T).name() + ".");
-        }
-        ldtk_error("Field \"" + name + "\" does not exist.");
-    }
-
-    template <FieldType T>
-    auto FieldsContainer::getField(const std::string& name) const -> const Field<getFieldType<T>>& {
-        return getField<getFieldType<T>>(name);
-    }
-
-    template <typename T>
     void FieldsContainer::addArrayField(const std::string& name, const std::vector<Field<T>>& field) {
         auto* new_field = new ArrayField<T>(field);
         m_array_fields[name] = new_field;
@@ -148,31 +113,4 @@ namespace ldtk {
         m_array_fields[name] = new_field;
         m_gc.emplace_back(new_field);
     }
-
-    template <typename T>
-    auto FieldsContainer::getArrayField(const std::string& name) const -> const ArrayField<T>& {
-        if (m_array_fields.count(name) > 0) {
-            const auto* field = m_array_fields.at(name);
-            const auto* ret = dynamic_cast<const ArrayField<T>*>(field);
-            if (ret)
-                return *ret;
-            ldtk_error("ArrayField \"" + name + "\" is not of type " + typeid(T).name() + ".");
-        }
-        ldtk_error("ArrayField \"" + name + "\" does not exist.");
-    }
-
-    template <FieldType T>
-    auto FieldsContainer::getArrayField(const std::string& name) const -> const ArrayField<getFieldType<T>>& {
-        return getArrayField<getFieldType<T>>(name);
-    }
-
-}
-
-template <typename T>
-auto operator<<(std::ostream& os, ldtk::Field<T> field) -> std::ostream& {
-    if (field.is_null())
-        os << "null";
-    else
-        os << field.value();
-    return os;
 }
