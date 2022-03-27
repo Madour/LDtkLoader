fe250402dca9b98b5f23c22f211de3b15519bda7
Modar Nasser <modar1999@gmail.com>
2022-03-27 18:17:34 +0200

    update raylib example

--- examples/Raylib/main.cpp
+++ examples/Raylib/main.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
-#include <LDtkLoader/World.hpp>
+#include <LDtkLoader/Project.hpp>
 #include <raylib.h>
 
-int main()
-{
-    // Declare a LDtk World.
-    ldtk::World world;
+int main() {
+    // declare a LDtk World
+    ldtk::Project ldtk_project;
 
-    // Load the LDtk World from file.
-    try
-    {
-        world.loadFromFile("assets/level.ldtk");
+    // load the LDtk World from file
+    try {
+        ldtk_project.loadFromFile("assets/level.ldtk");
     }
-    catch (const std::exception &ex)
-    {
+    catch (std::exception& ex) {
         std::cerr << ex.what() << std::endl;
-        return (1);
+        return 1;
     }
 
-    // Get the level and the layer we want to render.
-    const auto &level = world.getLevel("Level");
-    const auto &layer = level.getLayer("Ground");
+    // get the world
+    const auto& world = ldtk_project.getWorld();
 
-    // Get all the tiles in the Ground layer.
-    const auto &tiles_vector = layer.allTiles();
+    // get the level and the layer we want to render
+    const auto& level = world.getLevel("Level");
+    const auto& layer = level.getLayer("Ground");
+    // get all the tiles in the Ground layer
+    const auto& tiles_vector = layer.allTiles();
 
     // Init Raylib and create a window.
     InitWindow(level.size.x * 4, level.size.y * 4, "LDtkLoader - Raylib");
@@ -32,27 +31,28 @@ int main()
     // Load the texture and the renderer.
     Texture2D texture = LoadTexture(("assets/" + layer.getTileset().path).c_str());
     RenderTexture2D renderer = LoadRenderTexture(level.size.x, level.size.y);
-    while (!WindowShouldClose())
-    {
-        // Draw all the tiles.
-        BeginTextureMode(renderer);
-        ClearBackground(BLACK);
-        for (const auto &tile : tiles_vector)
-        {
-            Vector2 dest = {
-                static_cast<float>(tile.position.x),
-                static_cast<float>(tile.position.y),
-            };
-            Rectangle src = {
-                static_cast<float>(tile.texture_position.x),
-                static_cast<float>(tile.texture_position.y),
-                layer.getTileset().tile_size * ((tile.flipX == true) ? -1.0f : 1.0f),
-                layer.getTileset().tile_size * ((tile.flipY == true) ? -1.0f : 1.0f)
-            };
-            DrawTextureRec(texture, src, dest, WHITE);
-        }
-        EndTextureMode();
 
+    // Draw all the tiles.
+    BeginTextureMode(renderer);
+    ClearBackground(BLACK);
+    for (const auto &tile : tiles_vector) {
+        const auto& position = tile.getPosition();
+        const auto& texture_rect = tile.getTextureRect();
+        Vector2 dest = {
+            static_cast<float>(position.x),
+            static_cast<float>(position.y),
+        };
+        Rectangle src = {
+            static_cast<float>(texture_rect.x),
+            static_cast<float>(texture_rect.y),
+            static_cast<float>(texture_rect.width) * (tile.flipX ? -1.0f : 1.0f),
+            static_cast<float>(texture_rect.height) * (tile.flipY ? -1.0f : 1.0f)
+        };
+        DrawTextureRec(texture, src, dest, WHITE);
+    }
+    EndTextureMode();
+
+    while (!WindowShouldClose()) {
         // Scale up the Renderer times 4.
         // NOTE: Render texture must be y-flipped due to default OpenGL coordinates (left-bottom)
         BeginDrawing();
