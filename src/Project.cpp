0d7646843dd7ad6ffcc4f48f996d244f95851f41
Modar Nasser <modar1999@gmail.com>
2024-02-15 23:41:18 +0100

    add missing end of line after warning message

--- src/Project.cpp
+++ src/Project.cpp
@@ -181,7 +181,7 @@ void Project::load(const nlohmann::json& j, const FileLoader& file_loader, bool 
             std::cout << "LDtkLoader Warning: Version mismatch. LDtkLoader v" << api_major_minor
                       << " may fail to load the project \"" << getFilePath().c_str()<< "\" created"
                       << " with LDtk v" << json_major_minor << ". Please make sure your software is"
-                      << " up to date.";
+                      << " up to date." << std::endl;
         }
     }
 
